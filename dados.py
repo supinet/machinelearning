6b60e3796a54d4f6b4c018ec4343cd4cc62df3b7
Fernando <fernando.supi@gmail.com>
2017-09-19 13:01:20 -0400

    using pandas

--- dados.py
+++ dados.py
@@ -15,3 +15,18 @@ def access_load():
         X.append(data)
         Y.append(int(comprou))
     return X, Y
+
+def searches_load():
+    X = [];
+    Y = [];
+
+    file = open('busca.csv', 'rb')
+    reader = csv.reader(file)
+    reader.next()
+
+    for home,busca,logado,comprou in reader:
+        data = [int(home), busca, int(logado)]
+        X.append(data)
+        Y.append(int(comprou))
+
+    return X,Y
---
+++ searches_classify.py
@@ -0,0 +1,46 @@
+import pandas as pd
+
+df = pd.read_csv('busca.csv')
+
+#df = dataFrame
+X_df = df[['home', 'busca', 'logado']]
+Y_df = df['comprou']
+
+Xdummies_df = pd.get_dummies(X_df).astype(int)
+Ydummies_df = Y_df # quando tem somente uma coluna
+
+X = Xdummies_df.values
+Y = Ydummies_df.values
+
+fit_percentage = 0.9
+
+#tamaho de treino 90% da coluna de comprou
+#testes 100 registros = 10%
+fit_length = fit_percentage * len(Y)
+test_length = len(Y) - fit_length
+
+#dados do treino e #demarcation
+fit_data = X[:int(fit_length)]
+fit_demarcation = Y[:int(fit_length)]
+
+#test
+test_data = X[-int(test_length):]
+test_demarcation = Y[-int(test_length):]
+
+
+from sklearn.naive_bayes import MultinomialNB
+model = MultinomialNB()
+model.fit(fit_data, fit_demarcation)
+
+result = model.predict(test_data)
+differences = result - test_demarcation
+
+hits = [d for d in differences if d == 0]
+total_hits = len(hits)
+total_elements = len(test_data)
+
+hits_rate = 100.0 * total_hits / total_elements
+
+print(hits_rate)
+print(total_elements)
+
