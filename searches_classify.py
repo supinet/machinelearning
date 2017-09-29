42aa680a49138eedc2e05ab7e2cdac90ed8480db
Fernando <fernando.supi@gmail.com>
2017-09-29 13:02:09 -0400

    added adaBoost algorithm

--- searches_classify.py
+++ searches_classify.py
@@ -1,7 +1,8 @@
 from collections import Counter
 import pandas as pd
 
-df = pd.read_csv('busca.csv')
+#df = pd.read_csv('busca.csv')
+df = pd.read_csv('busca2.csv')
 
 #df = dataFrame
 X_df = df[['home', 'busca', 'logado']]
@@ -28,26 +29,32 @@ fit_demarcation = Y[:int(fit_length)]
 test_data = X[-int(test_length):]
 test_demarcation = Y[-int(test_length):]
 
+def fit_and_predict(model, fit_data, fit_demarcation, test_data, test_demarcation):
+    model.fit(fit_data, fit_demarcation)
+
+    result = model.predict(test_data)
+    hits = (result == test_demarcation)
+
+    # isn't necessary anymore
+    #hits = [d for d in differences if d == 0]
+    total_hits = sum(hits)
+    total_elements = len(test_data)
+
+    hits_rate = 100.0 * total_hits / total_elements
+
+    print('Algorithm Base hits rate: %f' % hits_rate)
 
 from sklearn.naive_bayes import MultinomialNB
 model = MultinomialNB()
-model.fit(fit_data, fit_demarcation)
+fit_and_predict(model, fit_data, fit_demarcation, test_data, test_demarcation)
 
-result = model.predict(test_data)
-hits = (result == test_demarcation)
-
-# isn't necessary anymore
-#hits = [d for d in differences if d == 0]
-total_hits = sum(hits)
-total_elements = len(test_data)
-
-hits_rate = 100.0 * total_hits / total_elements
-
-print('Algorithm Base hits rate: %f' % hits_rate)
-print(total_elements)
+from sklearn.ensemble import AdaBoostClassifier
+model = AdaBoostClassifier()
+fit_and_predict(model, fit_data, fit_demarcation, test_data, test_demarcation)
 
 #effectiveness test chuta um unico valor
 base_hits = max(Counter(test_demarcation).itervalues())
 
 base_hits_rate = 100.0 * base_hits / len(test_demarcation)
 print('Base hits rate: %f' % base_hits_rate)
+print("Total tests: %f " % len(test_data))
